5220ec45fdcbaed427c8e024de6cfb27e4e76d2c
chachagof <chacha0932@gmail.com>
2023-02-24 17:41:16 +0800

    feat:modify router and build login & register page

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const routes = require('./routes/index')
 
 const app = express()
 const port = 3000
 
-app.engine('hbs',exphbs.engine({defaultLayout:'main',extname:'.hbs'}))
-app.set('view engine','hbs')
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/',(req,res)=>{
-  res.send('hellow world')
-})
-
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`It's working on http://localhost:${port}/`)
 })
