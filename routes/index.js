c0407b35f3932d0bafad9e2df2e3dcbfe82d9c45
chachagof <chacha0932@gmail.com>
2023-02-25 15:08:38 +0800

    feat: add detail page and route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const user = require('./modules/user')
+const todo = require('./modules/todo')
 
 router.use('/users',user)
+router.use('/todos',todo)
 router.use('/',home)
 
 module.exports = router
