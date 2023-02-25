139f35537e32da57da3c05d95e73408730eefc9b
chachagof <chacha0932@gmail.com>
2023-02-25 18:40:57 +0800

    feat:built create route

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -5,11 +5,28 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+// create
+router.get('/new',(req,res)=>{
+  return res.render('new')
+})
+
+router.post('/',(req,res)=>{
+  const UserId = req.user.id
+  const {name} = req.body
+  Todo.create({ name , UserId})
+    .then(()=>res.redirect('/'))
+    .catch(err=>console.log(err))
+})
+// detail
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => {res.render('detail', { todo: todo.toJSON() })})
     .catch(error => console.log(error))
 })
+
+// edit
+
+// delete
 
 module.exports = router
