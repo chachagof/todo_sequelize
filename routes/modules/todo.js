c2349cc5911c84ae614c33a617d7ca8e7c50335b
chachagof <chacha0932@gmail.com>
2023-02-25 19:41:37 +0800

    feat:add update function

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -26,7 +26,31 @@ router.get('/:id', (req, res) => {
 })
 
 // edit
+router.get('/:id/edit',(req,res)=>{
+  const UserId = req.user.id
+  const id = req.params.id
 
+  Todo.findOne({where:{id,UserId}})
+    .then(todo=>res.render('edit', { todo: todo.toJSON() }))
+    .catch(err=>console.log(err))
+})
+
+router.put('/:id',(req,res)=>{
+  const UserId = req.user.id
+  const id = req.params.id
+  const {name,isDone} = req.body
+
+  return Todo.findOne({where:{id,UserId}})
+    .then(todo=>{
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => {
+      console.log(`/todos/${id}`)
+      return res.redirect(`/todos/${id}`)})
+    .catch(err => console.log(err))
+})
 // delete
 
 module.exports = router
