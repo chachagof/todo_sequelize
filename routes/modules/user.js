c6838bc1f949a214c7c754b9b77ee210efec2158
chachagof <chacha0932@gmail.com>
2023-02-25 14:23:16 +0800

    feat:add relations between user & todo models

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
 // login 
 router.get('/login',(req,res)=>{
   res.render('login')
@@ -14,7 +18,9 @@ router.get('/register',(req,res)=>{
   res.render('register')
 })
 router.post('/register',(req,res)=>{
-  res.send('register')
+  const { name , email , password , confirmPassword } = req.body
+  User.create({ name , email , password })
+    .then(user=>res.redirect('/'))
 })
 
 // logout
